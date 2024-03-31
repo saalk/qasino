b4630f94110c978f10678eb01cd29bee6155444b
saalk <vanndermeulen.k@gmail.com>
2024-03-31 16:26:00 +0200

    fix startup

--- server/webapp/src/main/java/cloud/qasino/games/core/context/ChannelContextException.java
+++
@@ -1,8 +0,0 @@
-package cloud.qasino.games.core.context;
-public class ChannelContextException extends Exception {
-    private static final long serialVersionUID = -4556823571855678805L;
-
-    public ChannelContextException(Throwable cause) {
-        super(cause);
-    }
-}
