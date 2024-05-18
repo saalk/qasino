12cb08142de29b8e8f24659a1926e2481d484fce
saalk <vanndermeulen.k@gmail.com>
2024-05-19 01:50:50 +0200

    thyme

--- server/webapp/src/main/java/cloud/qasino/games/database/repository/VisitorRepository.java
+++ server/webapp/src/main/java/cloud/qasino/games/database/repository/VisitorRepository.java
@@ -1,6 +1,6 @@
 package cloud.qasino.games.database.repository;
 
-import cloud.qasino.games.database.entity.Visitor;
+import cloud.qasino.games.database.security.Visitor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
