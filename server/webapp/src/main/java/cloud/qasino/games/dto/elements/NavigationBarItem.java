a065fd70fea74feb198b62de18d1e7328f523031
saalk <vanndermeulen.k@gmail.com>
2024-05-19 17:43:06 +0200

    thyme

--- server/webapp/src/main/java/cloud/qasino/games/dto/elements/NavigationBarItem.java
+++ server/webapp/src/main/java/cloud/qasino/games/dto/elements/NavigationBarItem.java
@@ -2,6 +2,7 @@ package cloud.qasino.games.dto.elements;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.voodoodyne.jackson.jsog.JSOGGenerator;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,9 +13,13 @@ import lombok.Setter;
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class NavigationBarItem {
 
+    @JsonProperty("intSequence")
     private int itemSequence;
+    @JsonProperty("visible")
     private boolean itemVisible;
+    @JsonProperty("name")
     private String itemName;
+    @JsonProperty("stats")
     private String itemStats;
 
 }
